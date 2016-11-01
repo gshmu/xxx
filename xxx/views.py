1c274070b82e0aaf5d65494038796b0fd23c5260
lugang <lugang@intra.nsfocus.com>
2016-11-01 09:52:23 +0800

    refine

--- xxx/views.py
+++ xxx/views.py
@@ -54,5 +54,5 @@ def M500(request, name, phone):
 
 def random_20x(request):
     now = datetime.now().time()
-    code = 200 + (now.hour + now.minute / 20) % 2
+    code = 200 if (now.hour + now.minute / 20) % 2 == 0 else 451
     return HttpResponse(status=code)
